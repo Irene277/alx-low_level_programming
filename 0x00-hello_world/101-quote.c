e43e6bd15c383f3bfca4dd85ce06e2c13ab152a0
Irene277 <Tonskiey2k@yahoo.com>
2022-07-07 04:05:53 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -9,6 +10,8 @@
  */
 int main(void)
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length = strlen(text);
+write(2, text, length);
 return (1);
 }
